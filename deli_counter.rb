2a5ae927788f9d1b51270eec95ca24752188c9f3
Ely Karr <elykarr@gmail.com>
2022-01-19 20:00:20 -0800

    commit tree 0fdd76d6970ac45e9ddb795e24f91976d6e7ce9c

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else 
+        # line_intro = "The line is currently:"
+        # katz_deli.each.with_index {|person, index| line_intro << " #{index +1}. #{person}"}
+        # puts line_intro
+        puts "The line is currently:#{(katz_deli.map.with_index {|person,index| " #{index + 1}. #{person}"}).join}"
+        # puts "The line is currently: #{katz_deli.length}. #{katz_deli.each.with_index do
+        # |person, index| puts {index} {person}}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length +1} in line."
+end
